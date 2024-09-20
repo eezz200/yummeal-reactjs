b37205624fcd1f1da79b2213f40dfac263b55712
ezz200 <endahsulistyawati73@gmail.com>
2024-09-20 23:08:45 +0700

    Initial commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,11 +12,11 @@ function NavBar() {
       <nav className="navbar">
         <div className="nav-container">
           <NavLink exact to="/" className="nav-logo">
-            <span>BAZAAR</span>
-            {/* <i className="fas fa-code"></i> */}
             <span className="icon">
               <CodeIcon />
             </span>
+            <span><h3>YUMMEAL BAZAR RPL 1</h3></span>
+            {/* <i className="fas fa-code"></i> */}
           </NavLink>
 
           <ul className={click ? "nav-menu active" : "nav-menu"}>
@@ -43,41 +43,8 @@ function NavBar() {
               </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink
-                exact
-                to="/blog"
-                activeClassName="active"
-                className="nav-links"
-                onClick={handleClick}
-              >
-                Blog
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                exact
-                to="/contact"
-                activeClassName="active"
-                className="nav-links"
-                onClick={handleClick}
-              >
-                Contact Us
-              </NavLink>
             </li>
           </ul>
-          <div className="nav-icon" onClick={handleClick}>
-            {/* <i className={click ? "fas fa-times" : "fas fa-bars"}></i> */}
-
-            {click ? (
-              <span className="icon">
-                <HamburgetMenuOpen />{" "}
-              </span>
-            ) : (
-              <span className="icon">
-                <HamburgetMenuClose />
-              </span>
-            )}
-          </div>
         </div>
       </nav>
     </>
--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Home= () => {
   return (
     <div>
-      <h1>Zyifa</h1>
+      <h1>LET'S SEE WHAT WE HAVE FOR YOU!</h1>
     </div>
   );
 };
